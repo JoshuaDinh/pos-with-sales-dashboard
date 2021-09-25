57cf59ba56c79f9bac9a4a5ab021bcc25716bad7
Joshua Dinh <joshuahungdinh@gmail.com>
2021-09-25 13:29:38 -0700

    updated styling

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,15 @@
 import React from "react";
 import "./app.css";
 import CheckOut from "./Pages/CheckOut/CheckOut";
+import Dashboard from "./Pages/Dashboard/Dashboard";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 const App = () => {
   return (
     <Router>
       <Switch>
-        <Route path="/" component={CheckOut} />
+        <Route path="/CheckOut" component={CheckOut} />
+        <Route path="/Dashboard" component={Dashboard} />
       </Switch>
     </Router>
   );

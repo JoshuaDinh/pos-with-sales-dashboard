d1df3bf3c9c8734ada8e4bacd610d6c1b9204cbc
Joshua Dinh <joshuahungdinh@gmail.com>
2021-09-26 10:02:50 -0700

    updated plans to subscriptions

---
+++ routes/api/subscriptions.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const connection = require("../../db/dbConfig");
+
+const router = express.Router();
+
+router.post("/", (req, res) => {
+  const { name, revenue, speed, cost } = req.body;
+
+  const sql =
+    "INSERT INTO plans (name, revenue, speed, cost) VALUES (?, ?, ?, ?)";
+  const data = [name, revenue, speed, cost];
+
+  connection.query(sql, data, (err, result) => {
+    if (err) {
+      res.status(500).json({ error: err });
+    } else {
+      res.status(200).json(result);
+    }
+  });
+});
+
+router.get("/", (req, res) => {
+  const sql = "SELECT * FROM PLANS";
+  connection.query(sql, (err, result) => {
+    if (err) {
+      res.status(500).json({ error: err });
+    } else {
+      res.status(200).json(result);
+    }
+  });
+});
+
+module.exports = router;

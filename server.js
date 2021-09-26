167ebc8769dc0783a33169050914fd9f05367247
Joshua Dinh <joshuahungdinh@gmail.com>
2021-09-25 20:29:46 -0700

    added plan get routes

--- server.js
+++ server.js
@@ -10,7 +10,5 @@ const PORT = process.env.PORT || "5000";
 app.use(express.json({ extended: false }));
 
 // Routes
-
 app.use("/api/plans", planRoutes);
-
 app.listen(PORT, () => console.log(`Connected on port : ${PORT}`));

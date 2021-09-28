000598965e0e5a8b74b364ee5e26702d73500967
Joshua Dinh <joshuahungdinh@gmail.com>
2021-09-27 22:13:41 -0700

    updated coloring

--- client/src/Components/Sidebar/Sidebar.js
+++ client/src/Components/Sidebar/Sidebar.js
@@ -9,13 +9,25 @@ const Sidebar = () => {
   return (
     <div className="sidebar">
       <nav className="nav-container">
-        <NavLink to="/home" className="sidebar-link">
+        <NavLink
+          to="/home"
+          className="sidebar-link"
+          activeClassName="sidebar-active"
+        >
           <HomeIcon className="navLink-icon" />
         </NavLink>
-        <NavLink to="/CheckOut" className="sidebar-link">
+        <NavLink
+          to="/CheckOut"
+          className="sidebar-link"
+          activeClassName="sidebar-active"
+        >
           <PointOfSaleIcon className="navLink-icon" />
         </NavLink>
-        <NavLink to="/Dashboard" className="sidebar-link">
+        <NavLink
+          to="/Dashboard"
+          className="sidebar-link"
+          activeClassName="sidebar-active"
+        >
           <AssessmentIcon className="navLink-icon" />
         </NavLink>
       </nav>

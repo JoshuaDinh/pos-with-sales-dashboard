3438f997aa17859af39b34d049857f947d474b93
Joshua Dinh <joshuahungdinh@gmail.com>
2021-09-29 09:09:11 -0700

    added datasets for totals subscription sales

--- client/src/Components/DataCard/DataCard.js
+++ client/src/Components/DataCard/DataCard.js
@@ -1,12 +1,12 @@
 import React from "react";
 import "./dataCard.css";
 
-const DataCard = ({ title, amount, percent }) => {
+const DataCard = ({ title, amount, subTitle }) => {
   return (
     <div className="data-card">
       <h4>{title}</h4>
       <h3>{amount}</h3>
-      <span>{percent}</span>
+      <p>{subTitle}</p>
     </div>
   );
 };

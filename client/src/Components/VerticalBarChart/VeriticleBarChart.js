3de79311ed0f579178e1c54bfacf5fda9cff7c80
Joshua Dinh <joshuahungdinh@gmail.com>
2021-09-29 09:09:58 -0700

    updated with date formating and yearly sales by the month datasets

--- client/src/Components/VerticalBarChart/VeriticleBarChart.js
+++ client/src/Components/VerticalBarChart/VeriticleBarChart.js
@@ -1,39 +1,9 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./verticalBar.css";
 import { Bar } from "react-chartjs-2";
-
-const data = (canvas) => {
-  const ctx = canvas.getContext("2d");
-  const gradient = ctx.createLinearGradient(0, 0, 240, 240);
-  gradient.addColorStop(0.1, "rgb(255, 200, 55,0.6)");
-  gradient.addColorStop(0.5, "rgb(255, 128, 8)");
-  gradient.addColorStop(0.8, "rgb(255, 200, 55,0.6)");
-  gradient.addColorStop(1, "rgb(255, 128, 8)");
-  return {
-    labels: [
-      "Jan",
-      "Feb",
-      "Mar",
-      "Apr",
-      "May",
-      "Jun",
-      "Aug",
-      "Sep",
-      "Oct",
-      "Nov",
-      "Dec",
-    ],
-    datasets: [
-      {
-        label: "# of Votes",
-        data: [12, 19, 3, 5, 2, 3],
-        backgroundColor: gradient,
-        borderColor: "#000000",
-        borderWidth: 1,
-      },
-    ],
-  };
-};
+import { fetchTotalYearlyDataByMonth } from "../../Actions/totalSalesData";
+import { connect } from "react-redux";
+import moment from "moment";
 
 const options = {
   maintainAspectRatio: false,
@@ -49,10 +19,44 @@ const options = {
   },
 };
 
-const VerticalBar = () => (
-  <div className="vertical-bar">
-    <Bar data={data} options={options} />
-  </div>
-);
+const VerticalBar = ({ dataSet }) => {
+  const data = (canvas) => {
+    const ctx = canvas.getContext("2d");
+    const gradient = ctx.createLinearGradient(0, 0, 240, 240);
+    gradient.addColorStop(0.1, "rgb(255, 200, 55,0.6)");
+    gradient.addColorStop(0.5, "rgb(255, 128, 8)");
+    gradient.addColorStop(0.8, "rgb(255, 200, 55,0.6)");
+    gradient.addColorStop(1, "rgb(255, 128, 8)");
+
+    const label_names = [];
+    const chart_data = [];
+    const get_label_names = dataSet.map((data) => {
+      label_names.push(moment(data.new_date).format("MMM yy "));
+    });
+
+    const get_chart_data = dataSet.map((data) => {
+      chart_data.push(data["SUM(subscription_price)"]);
+    });
+
+    return {
+      labels: label_names,
+      datasets: [
+        {
+          label: "Total Sales By Month",
+          data: chart_data,
+          backgroundColor: gradient,
+          borderColor: "#000000",
+          borderWidth: 1,
+        },
+      ],
+    };
+  };
+
+  return (
+    <div className="vertical-bar">
+      <Bar data={data} options={options} />
+    </div>
+  );
+};
 
 export default VerticalBar;

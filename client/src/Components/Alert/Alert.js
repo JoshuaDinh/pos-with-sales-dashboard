9630f1d68a4fc0c922640c97c55e1100ec808d3f
Joshua Dinh <joshuahungdinh@gmail.com>
2021-09-29 13:14:32 -0700

    test deploy

--- client/src/Components/Alert/Alert.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "./alert.css";
-
-const Alert = (title) => {
-  return <div className="alert">{title}</div>;
-};
-
-export default Alert;
--- client/src/Pages/Dashboard/Dashboard.js
+++ client/src/Pages/Dashboard/Dashboard.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect } from "react";
 import "./dashboard.css";
 import { connect } from "react-redux";
 import Sidebar from "../../Components/Sidebar/Sidebar";

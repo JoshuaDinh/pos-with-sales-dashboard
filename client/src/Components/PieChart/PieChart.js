70d54f02b902beecf777cd9f2e0dcb5008ae7637
Joshua Dinh <joshuahungdinh@gmail.com>
2021-09-29 09:06:41 -0700

    added todays subscription sales to chart

--- client/src/Components/PieChart/PieChart.js
+++ client/src/Components/PieChart/PieChart.js
@@ -3,34 +3,49 @@ import "./pieChart.css";
 
 import { Pie } from "react-chartjs-2";
 
-const data = (canvas) => {
-  const ctx = canvas.getContext("2d");
-
-  let gradient = ctx.createLinearGradient(0, 0, 240, 240);
-  gradient.addColorStop(0.1, "rgb(255, 200, 55,0.6)");
-  gradient.addColorStop(0.5, "rgb(255, 128, 8)");
-  gradient.addColorStop(0.8, "rgb(255, 200, 55,0.6)");
-  gradient.addColorStop(1, "rgb(255, 128, 8)");
-
-  return {
-    labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
-    datasets: [
-      {
-        label: "# of Votes",
-        data: [12, 19, 3, 5, 3],
-        backgroundColor: gradient,
-        borderColor: "#000000",
-        borderWidth: 1,
-      },
-    ],
-  };
-};
 const options = { maintainAspectRatio: false, responsive: true };
 
-const PieChart = () => (
-  <div className="pie-chart">
-    <Pie data={data} options={options} />
-  </div>
-);
+const PieChart = ({ dataSet }) => {
+  const data = (canvas) => {
+    const ctx = canvas.getContext("2d");
+
+    let gradient = ctx.createLinearGradient(0, 0, 240, 240);
+    gradient.addColorStop(0.1, "rgb(255, 200, 55,0.6)");
+    gradient.addColorStop(0.5, "rgb(255, 128, 8)");
+    gradient.addColorStop(0.8, "rgb(255, 200, 55,0.6)");
+    gradient.addColorStop(1, "rgb(255, 128, 8)");
+
+    const label_names = [];
+    const chart_data = [];
+    const get_label_names = dataSet.daily.map((data) => {
+      label_names.push(data.subscription_name);
+    });
+
+    const get_chart_data = dataSet.daily.map((data) => {
+      chart_data.push(data["COUNT(*)"]);
+    });
+
+    return {
+      labels: label_names,
+      datasets: [
+        {
+          label: "# of Votes",
+          data: chart_data,
+          backgroundColor: gradient,
+          borderColor: "#000000",
+          borderWidth: 1,
+        },
+      ],
+    };
+  };
+  return (
+    <div className="pie-chart-container">
+      <h3>Todays Subscriptions Sales</h3>
+      <div className="pie-chart">
+        <Pie data={data} options={options} />
+      </div>
+    </div>
+  );
+};
 
 export default PieChart;

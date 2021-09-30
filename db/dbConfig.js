4fcdd29c13704866c3f0d21585127c3be4bfed2e
Joshua Dinh <joshuahungdinh@gmail.com>
2021-09-30 07:58:49 -0700

    updated

--- db/dbConfig.js
+++ db/dbConfig.js
@@ -1,31 +1,31 @@
 const mysql = require("mysql");
 
-const connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "joshua123",
-  SSL: "enabled with TLS_AES_256_GCM_SHA384",
-  database: "ezoic",
-});
-
-// const connection = mysql.createPool({
-//   host: "us-cdbr-east-04.cleardb.com",
-//   user: "b9ce9353881a66",
-//   password: "5ba60be2",
-//   database: "heroku_7b28926ea541b90",
+// const connection = mysql.createConnection({
+//   host: "localhost",
+//   user: "root",
+//   password: "joshua123",
+//   SSL: "enabled with TLS_AES_256_GCM_SHA384",
+//   database: "ezoic",
 // });
 
-connection.connect((err, connection) => {
-  if (err) {
-    console.log(`error connecting : ${err}`);
-  }
-  console.log("Connected successfully to DB");
+const connection = mysql.createPool({
+  host: "us-cdbr-east-04.cleardb.com",
+  user: "b9ce9353881a66",
+  password: "5ba60be2",
+  database: "heroku_7b28926ea541b90",
 });
-// connection.getConnection((err, connection) => {
+
+// connection.connect((err, connection) => {
 //   if (err) {
 //     console.log(`error connecting : ${err}`);
 //   }
 //   console.log("Connected successfully to DB");
 // });
+connection.getConnection((err, connection) => {
+  if (err) {
+    console.log(`error connecting : ${err}`);
+  }
+  console.log("Connected successfully to DB");
+});
 
 module.exports = connection;

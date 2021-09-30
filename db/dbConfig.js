aa6b4e7120b98c2ffae8771f35f97e94a0c72a2d
Joshua Dinh <joshuahungdinh@gmail.com>
2021-09-30 09:58:16 -0700

    removed config for local setup

--- db/dbConfig.js
+++ db/dbConfig.js
@@ -1,13 +1,5 @@
 const mysql = require("mysql");
 
-// const connection = mysql.createConnection({
-//   host: "localhost",
-//   user: "root",
-//   password: "joshua123",
-//   SSL: "enabled with TLS_AES_256_GCM_SHA384",
-//   database: "ezoic",
-// });
-
 const connection = mysql.createPool({
   host: "us-cdbr-east-04.cleardb.com",
   user: "b9ce9353881a66",
@@ -15,12 +7,6 @@ const connection = mysql.createPool({
   database: "heroku_7b28926ea541b90",
 });
 
-// connection.connect((err, connection) => {
-//   if (err) {
-//     console.log(`error connecting : ${err}`);
-//   }
-//   console.log("Connected successfully to DB");
-// });
 connection.getConnection((err, connection) => {
   if (err) {
     console.log(`error connecting : ${err}`);

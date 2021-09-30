0bad4a0bb394372a308839b8cfb4146bbe007064
Joshua Dinh <joshuahungdinh@gmail.com>
2021-09-29 20:20:31 -0700

    updated connection pooling with proper connect function

--- db/dbConfig.js
+++ db/dbConfig.js
@@ -7,6 +7,7 @@ const mysql = require("mysql");
 //   SSL: "enabled with TLS_AES_256_GCM_SHA384",
 //   database: "ezoic",
 // });
+
 const connection = mysql.createPool({
   host: "us-cdbr-east-04.cleardb.com",
   user: "b9ce9353881a66",
@@ -14,9 +15,9 @@ const connection = mysql.createPool({
   database: "heroku_7b28926ea541b90",
 });
 
-connection.connect((err) => {
+connection.getConnection((err, connection) => {
   if (err) {
-    console.log(`error connecting : ${err.stack}`);
+    console.log(`error connecting : ${err}`);
   }
   console.log("Connected successfully to DB");
 });

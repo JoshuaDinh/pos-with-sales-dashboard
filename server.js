bc90665b2ba2456d630b282fe28f6225d78a6043
Joshua Dinh <joshuahungdinh@gmail.com>
2021-09-29 19:28:33 -0700

    updated

--- server.js
+++ server.js
@@ -12,20 +12,18 @@ const PORT = process.env.PORT || "5000";
 // Middleware - method to recognize the incoming Request Object as a JSON Object.
 app.use(express.json({ extended: false }));
 
-// Static assets - production
-// Static assets - production
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
-  });
-}
-
 // Routes
 app.use("/api/subscriptions", subscriptionRoutes);
 app.use("/api/employees", employeeRoutes);
 app.use("/api/invoices", invoiceRoutes);
 app.use("/api/sales", salesRoutes);
+// Static assets - production
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
 
-app.listen(PORT, () => console.log(`Connected on port : ${PORT}`));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
+app.listen(PORT, () => console.log(`app is running on ${PORT}`));
